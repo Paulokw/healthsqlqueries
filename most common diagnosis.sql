9f688e2482506d4b98b1bf6a39dba3ec49a24f71
Paulokw <108173012+Paulokw@users.noreply.github.com>
2023-09-26 16:20:40 +0300

    Add files via upload

---
+++ most common diagnosis.sql
@@ -0,0 +1,18 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+  TOP (1000) [Diagnosis], 
+  MedicalCenter, 
+  COUNT(diagnosis) as diagnosis_count
+FROM 
+  [AdventureWorksLT2022].[dbo].[BI_Analyst_Assessment_Data_2023] 
+  left join dbo.[BI_Analyst_Assessment_Data_2023 3] ON dbo.BI_Analyst_Assessment_Data_2023.VisitCode = dbo.[BI_Analyst_Assessment_Data_2023 3].VisitCode 
+where 
+  MedicalCenter IN ('Tassia', 'Embakasi') 
+group by 
+  Diagnosis, 
+  MedicalCenter 
+order by 
+  diagnosis_count desc
+
+
+  
